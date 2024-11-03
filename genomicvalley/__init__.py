4a2cdfe50ca670f85b442c7b6326277ab58840d9
Vishal <vishalonceagain@gmail.com>
2024-11-03 11:41:13 +0000

    added plant genomics page

--- genomicvalley/__init__.py
+++ genomicvalley/__init__.py
@@ -40,7 +40,8 @@ from .pages import (
     whole_exome,
     whole_genome,
     genetic_counseling,
-    faq
+    faq,
+    plant_genome
 )
 
 __all__ = [
@@ -85,5 +86,6 @@ __all__ = [
     "whole_exome",
     "whole_genome",
     "genetic_counseling",
-    "faq"
+    "faq",
+    "plant_genome"
 ]
