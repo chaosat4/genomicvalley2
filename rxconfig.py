2b2f343665f41a58cf6df795e32d6d0019893abb
Vishal <95615776+vishal7245@users.noreply.github.com>
2025-03-06 22:25:33 +0530

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="genomicvalley",
     db_url="sqlite:///reflex.db",
+    api_url="http://genomicvalley.com:8000",
     frontend_port=3001,
     backend_port=8001,
     cors_allowed_origins=["*"],
